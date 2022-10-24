5c3af012942e24a66aa2594110e53a7475f021af
berkay <berkayurun@outlook.com>
2022-10-24 13:35:46 +0300

    add Card to index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Attribution } from "./components/Attribution";
+import { Card } from "./components/Card";
 
 function App() {
   return (
-    <>
+    <main className="flex flex-col items-center justify-center bg-main-bg h-screen px-6">
+      <Card
+        nftName="Equilibrium #3429"
+        nftDescription="Our Equilibrium collection promotes balance and calm."
+        nftPrice="0.041 ETH"
+        nftTimeLeft="3 days left"
+      />
       <Attribution />
-    </>
+    </main>
   );
 }
 

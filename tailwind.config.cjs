1666aa1b84f84ad4e9b96017f54fbb63b104c64a
berkay <berkayurun@outlook.com>
2022-10-24 12:42:13 +0300

    setup project colors and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-soft-blue": "hsl(215, 51%, 70%)",
+        "primary-cyan": "hsl(178, 100%, 50%)",
+        "main-bg": "hsl(217, 54%, 11%)",
+        "card-bg": "hsl(216, 50%, 16%)",
+        "line-color": "hsl(215, 32%, 27%)",
+      },
+    },
   },
   plugins: [],
 };
